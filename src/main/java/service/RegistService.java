b471b7a41be4e5363cb0016bb7b908db88407f4d
dydtmd6801 <dydtmd6801@naver.com>
2023-05-23 17:07:35 +0900

    feat: 회원가입 서비스 기능
    duplicateUserId: 사용자 id를 기반으로 데이터베이스를 조회하여 중복체크 기능
    insertInfo: 사용자 정보를 데이터베이스에 저장

---
+++ src/main/java/service/RegistService.java
@@ -0,0 +1,41 @@
+package service;
+
+import com.sun.jdi.request.DuplicateRequestException;
+import domain.Member;
+import domain.Register;
+import model.MemberDao;
+
+import java.time.LocalDateTime;
+
+public class RegistService {
+
+    private MemberDao memberDao;
+
+    public RegistService(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    public int duplicateUserId(Register register) {
+        Member member = memberDao.findByUserId(register.getUserId());
+        if(member != null) {
+            throw new DuplicateRequestException("dup userId");
+        }
+        return 1;
+    }
+
+    public long insertInfo(Register register) {
+        if(register.getPassword() == register.getConfirmPassword()) {
+            Member newMember = new Member(
+                    register.getUserId(),
+                    register.getPassword(),
+                    register.getName(),
+                    register.getNickName(),
+                    register.getPhoneNumber(),
+                    register.getEmail(),
+                    LocalDateTime.now());
+            memberDao.insert(newMember);
+            return newMember.getId();
+        }
+        return 2;
+    }
+}
